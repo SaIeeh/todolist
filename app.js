080dcef5ab83897791c3d9112544685423e6322e
Saleh <9looo7y77@gmail.com>
2023-07-17 19:17:02 +0300

    second commit

--- app.js
+++ app.js
@@ -2,44 +2,33 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const https = require("https");
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+// Move the declaration of `day` to a higher scope
+
+var items = ["Buy food","Cook food","Eat food"];
 app.get("/", function(req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var option = {
+      weekday : "long" ,
+      day : "numeric" , 
+      month : "long" ,
+      year : "numeric"
+    };
+    var day = today.toLocaleDateString("ar-sa",option);
   
-    switch (currentDay) {
-        case 0:
-         day = "Sunday";
-          break;
-        case 1:
-            day = "Monday";
-          break;
-        case 2:
-            day = "Tuesday";
-          break;
-        case 3:
-            day = "Wednesday";
-          break;
-        case 4:
-            day = "Thursday";
-          break;
-        case 5:
-            day = "Friday";
-          break;
-        case 6:
-            day = "Saturday";
-          break;
-          default:
-    console.log("Invalid day");
-      }
-      res.render("list" , {day : day})
+    res.render("list", { day: day ,addNewListItem:items});
 });
 
+app.post("/", function(request, response) {
+    var item = request.body.add;
+    items.push(item);
+    response.redirect("/");
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
